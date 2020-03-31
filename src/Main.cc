88fea2d74e85c275a2fee2a6af64e8f29e4b1023
Louis de Ferluc <ldeferluc@gmail.com>
2020-03-31 19:10:58 +0200

    add open wbo

--- src/Main.cc
+++ src/Main.cc
@@ -7,4 +7,26 @@ int main(int argc, char** argv){
 
   cout << "Ces fdp de marseillais de con" << endl;
 
+  cout << "run unsat test ..." << endl;
+  vector<int> lits;
+  MaxSATSolver maxsat(3, 0);
+  lits = {1, 2};
+  maxsat.addClause(lits);
+  lits = {1, -2};
+  maxsat.addClause(lits);
+  lits = {-1, -2};
+  maxsat.addClause(lits);
+  lits = {-1, 2};
+  maxsat.addClause(lits);
+
+  std::vector<int> model;
+  uint64_t cost = 0;
+  MaxSATSolver::ReturnCode ret = maxsat.compute_maxsat(model, cost);
+
+  if(ret == MaxSATSolver::ReturnCode::SATISFIABLE)
+	cout << "satisfiable" << endl;
+  else if(ret == MaxSATSolver::ReturnCode::UNSATISFIABLE)
+	cout << "unsatisfiable" << endl;
+  else if(ret == MaxSATSolver::ReturnCode::UNKNOWN)
+	cout << "unknown" << endl;
 }
