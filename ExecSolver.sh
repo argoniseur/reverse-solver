ff7574aaa2d1230abf0bc4ff52947cf4409dc11a
Louis de Ferluc <ldeferluc@gmail.com>
2020-04-09 19:04:22 +0200

    Compilation fonctionnelle + script de démarrage pour gérer export

---
+++ ExecSolver.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -f "src/ReverseSolver" ]
+then
+	echo "You should compile first ! Use the make command, or see the readme if it's your first use :)"
+	exit 99
+fi
+
+oldLDPath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`/resources/smax-master/lib"
+
+./src/ReverseSolver "$*"
+
+export LD_LIBRARY_PATH=$oldLDPath
